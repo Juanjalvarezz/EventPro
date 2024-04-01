c9d1197e411522fde405af5bba86778c054bf70a
Juan Alvarez <122636634+Juanjalvarezz@users.noreply.github.com>
2024-04-01 00:10:39 -0500

    Cargando cambios en cartas de Eventos

--- FrontEnd/EventPro/src/components/Solicitudes/Solicitudes.jsx
+++ FrontEnd/EventPro/src/components/Solicitudes/Solicitudes.jsx
@@ -27,33 +27,33 @@ function Solicitudes() {
 
   return (
     <>
+    <h2 className='bg-gradient-to-r from-complement-800 to-primary-600 montserrat w-fit text-3xl my-6 font-black text-center py-3 px-6  rounded-xl'>Solicitudes de Eventos</h2>
       {isLoading ? (
         <Loading />
       ) : (
         <>
-          <h2 className='bg-gradient-to-r from-complement-800 to-primary-600 montserrat text-3xl w-xl my-6 font-black text-center py-1 px-4 rounded-xl'>Solicitudes de Eventos</h2>
           {promotorEvents && (
             promotorEvents.map(event => (
-              <div key={event._id} className="montserrat font-bold w-11/12 md:w-3/4 lg:w-1/2 mb-6 text-xs md:text-sm lg:text-md">
-                <div className="bg-primary-750 flex flex-col justify-center items-center mx-auto p-4 rounded-t-3xl">
-                  <div className="flex justify-between">
-                    <div className="flex flex-col items-center justify-center flex-1">
-                      <div className="flex flex-col items-center justify-center py-2 px-4 w-11/12 rounded-xl bg-gradient-to-r from-[#1E40AF] to-primary-600 mb-2">
-                        <p>Promotor: {event.promotor.name}</p>
-                        <p>Email: {event.promotor.email}</p>
-                      </div>
-                      <div className="text-center flex flex-col">
-                        <p>Evento: {event.name}</p>
-                        <p>Ubicación: {event.place}</p>
-                        <p>Fecha: 06 Julio</p>
-                        <div className="flex justify-center">
-                          <p>Estatus: </p>
-                          <p className={`ml-2 ${event.status === 'Disponible' ? 'text-green-600' : 'text-amber-500'}`}>{event.status}</p>
-                        </div>
-                      </div>
+              <div key={event._id} className="montserrat font-bold lg:w-3/5  sm:w-11/12 mb-6 text-xs md:text-sm lg:text-md">
+                <div className="bg-primary-750 mx-auto p-6 rounded-t-3xl">
+                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-3">
+
+                  <div className="md:flex md:justify-center md:items-center md:flex-1">
+                      <img className="lg:w-96 md:w-80 sm:w-72 rounded-3xl mx-auto" src={event.image} alt={event.name} />
                     </div>
-                    <div className="flex justify-end items-center flex-1">
-                      <img className="max-h-36" src={event.image} alt={event.name} />
+
+                    <div className="md:flex md:flex-col md:items-center md:justify-center md:flex-1">
+                      <div className="text-center md:text-center">
+                        <p className="text-xl sm:text-2xl md:text-2xl lg:text-2xl text-[#CAA8F5] mb-4">{event.name}</p>
+                        <p className="sm:text-lg md:text-lg lg:text-xl">Ubicación: {event.place}</p>
+                        <p className="sm:text-lg md:text-lg lg:text-xl">Fecha: {new Date(event.date).toLocaleDateString()}</p>
+                      </div>
+
+                    <div className="flex justify-center md:justify-start">
+                          <p className="text-sm sm:text-lg md:text-lg lg:text-xl">Estatus: </p>
+                          <p className={`ml-2 ${event.status === 'Disponible' ? 'text-green-600' : 'text-amber-500'} text-sm sm:text-lg md:text-lg lg:text-xl`}>{event.status}</p>
+                    </div>
+
                     </div>
                   </div>
                 </div>
@@ -61,8 +61,8 @@ function Solicitudes() {
                   {user.role === 'admin' && (
                     <button className="bg-blue-500 w-36 rounded-xl p-2">Aceptar</button>
                   )}
-                  <button className="bg-red-500 w-36 rounded-xl p-2">{`${user.role === 'admin' ? 'Rechazar' : 'Cancelar'}`}</button>
-                </div>
+                  <button className="bg-red-500 w-36 rounded-xl p-2 sm:text-lg md:text-xl lg:text-2xl">{`${user.role === 'admin' ? 'Rechazar' : 'Cancelar'}`}</button>
+                </div> 
               </div>
             ))
           )}
