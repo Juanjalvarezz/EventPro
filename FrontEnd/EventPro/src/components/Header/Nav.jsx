d0fff90fdf96842fc71446c492c092be692d11c2
mendezleoa <leonardoamm1100@gmail.com>
2024-04-09 16:35:30 -0400

    Cambios al mode switch

--- FrontEnd/EventPro/src/components/Header/Nav.jsx
+++ FrontEnd/EventPro/src/components/Header/Nav.jsx
@@ -11,7 +11,7 @@ const NavLinks = ({ categories, handleLogout, setCategories }) => {
       )
     );
   };
-  
+
   return (
     <>
       {categories.map((category, index) => (
@@ -37,11 +37,71 @@ const NavLinks = ({ categories, handleLogout, setCategories }) => {
   );
 };
 
-const Nav = () => {
-  const [isOpen, setIsOpen] = useState(false);
+const Switch = () => {
   const [theme, setTheme] = useState(
     localStorage.getItem("theme") ? localStorage.getItem("theme") : ""
   ); //Tema predeterminado
+  useEffect(() => {
+    if (!localStorage.getItem("theme")) {
+      setTheme("dark");
+    }
+    if (localStorage.getItem("theme") === "dark") {
+      setTheme("dark");
+    }
+    if (localStorage.getItem("theme") === "light") {
+      setTheme("light");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.querySelector("html")?.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      document.querySelector("html")?.classList.remove("light");
+    }
+    if (theme === "light") {
+      document.querySelector("html")?.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+      document.querySelector("html")?.classList.add("light");
+    }
+  }, [theme]);
+
+  function toggleDarkMode() {
+    setTheme(theme === "light" ? "dark" : "light");
+    const root = document.querySelector("body");
+    root.classList.toggle("dark-mode");
+  }
+
+  return (
+    <>
+      <input
+        type="checkbox"
+        id="mode-switch"
+        className="hidden"
+        onChange={toggleDarkMode}
+        checked={theme === "dark" ? true : false}
+      />
+      <label
+        for="mode-switch"
+        className={
+          (theme === "dark"
+            ? "bg-opacity-70 bg-secondary-700"
+            : "bg-secondary-200 bg-opacity-50") +
+          " inline-flex relative border rounded-full p-2 transition-colors duration-300 ease-in-out"
+        }
+      >
+        <i
+          className={"dark:text-transparent text-yellow-400 fas fa-sun mr-2 transition-colors duration-300 ease-in-out"}
+        />
+        <i className={"dark:text-blue-400 text-transparent fas fa-moon ml-2 transition-colors duration-300 ease-in-out"} />
+      </label>
+    </>
+  );
+};
+
+const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const { logout, user, isAuthenticated } = useAuth(); // Obtiene el rol del usuario y el estado de autenticación desde el contexto de autenticación
 
   // Determina las categorías de navegación según el estado de autenticación y el rol del usuario
@@ -92,42 +152,13 @@ const Nav = () => {
     setIsOpen(!isOpen);
   };
 
-  function toggleDarkMode() {
-    setTheme(theme === "light" ? "dark" : "light");
-    const root = document.querySelector("body"); // Puedes usar 'body' en lugar de 'html' si prefieres
-    root.classList.toggle("dark-mode");
-  }
-  
-  useEffect(() => {
-    if (!localStorage.getItem("theme")) { // Si no hay tema almacenado
-      setTheme("dark"); // Establecer tema oscuro por defecto
-    }
-    if (localStorage.getItem("theme") === "dark") {
-      setTheme("dark");
-    }
-    if (localStorage.getItem("theme") === "light") {
-      setTheme("light");
-    }
-  }, []);
-  
-  useEffect(() => {
-    if (theme === "dark") {
-      document.querySelector("html")?.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-      document.querySelector("html")?.classList.remove("light");
-    }
-    if (theme === "light") {
-      document.querySelector("html")?.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-      document.querySelector("html")?.classList.add("light");
-    }
-  }, [theme]);
-  
-
   return (
     <>
       <nav className="flex w-1/3 justify-end">
-        <label className="inline-flex md:hidden mx-3 items-center cursor-pointer">
+        <div className="inline-flex md:hidden mx-3 items-center cursor-pointer">
+          <Switch />
+        </div>
+        {/* <label className="inline-flex md:hidden mx-3 items-center cursor-pointer">
           <input
             type="checkbox"
             onChange={toggleDarkMode}
@@ -135,9 +166,10 @@ const Nav = () => {
             checked={theme === "dark" ? true : false}
           />
           <div className="relative w-11 h-6 bg-secondary-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-primary-900 dark:peer-focus:ring-primary-700 rounded-full peer dark:bg-secondary-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-secondary-50 after:border-secondary-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-secondary-600 peer-checked:bg-secondary-600"></div>
-        </label>
+        </label> */}
         <div className="hidden w-full justify-end md:flex text-xl">
-          <label className="inline-flex mx-3 items-center cursor-pointer">
+          <Switch />
+          {/* <label className="inline-flex mx-3 items-center cursor-pointer">
             <input
               type="checkbox"
               onChange={toggleDarkMode}
@@ -145,14 +177,14 @@ const Nav = () => {
               checked={theme === "dark" ? true : false}
             />
             <div className="relative w-11 h-6 bg-secondary-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-primary-900 dark:peer-focus:ring-primary-700 rounded-full peer dark:bg-secondary-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-secondary-50 after:border-secondary-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-secondary-600 peer-checked:bg-secondary-600"></div>
-          </label>
+          </label> */}
 
-          <div className="lg:text-xl md:text-md sm:text-sm" >
-          <NavLinks
-            categories={categories}
-            handleLogout={logout}
-            setCategories={setCategories}
-          />
+          <div className="lg:text-xl md:text-md sm:text-sm">
+            <NavLinks
+              categories={categories}
+              handleLogout={logout}
+              setCategories={setCategories}
+            />
           </div>
         </div>
 
@@ -162,7 +194,9 @@ const Nav = () => {
       </nav>
 
       {isOpen && (
-        <div className="flex basis-full ml-10 flex-col items-center text-center space-y-1 mb-5 p-2 "> {/* Para el menu abierto */}
+        <div className="flex basis-full ml-10 flex-col items-center text-center space-y-1 mb-5 p-2 ">
+          {" "}
+          {/* Para el menu abierto */}
           <NavLinks categories={categories} handleLogout={logout} />
         </div>
       )}
